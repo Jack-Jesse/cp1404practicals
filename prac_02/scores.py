57c3db0d965c1b1c285cf093e11623ff81605185
Jack <jack.jesse@my.jcu.edu.au>
2022-09-23 15:32:06 +1000

    Add functions to program

--- prac_02/scores.py
+++ prac_02/scores.py
@@ -1,17 +1,34 @@
 """
 broken scores
 CP1404/CP5632 - Practical
-Broken program to determine score status
+A program to determine the result of a score.
 """
+import random
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+
+def main():
+    score = get_score()
+    result = determine_score(score)
+    random_score = random.randint(0, 100)
+    random_score_result = determine_score(random_score)
+    print(result)
+    print(random_score, random_score_result)
+
+
+def get_score():
+    score = float(input("Enter score: "))
+    return score
+
+
+def determine_score(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
