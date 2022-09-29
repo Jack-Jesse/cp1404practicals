9b5bc4caf9ef23e1dd01672a55d257643047610d
Jack <jack.jesse@my.jcu.edu.au>
2022-09-29 16:06:37 +1000

    Initial commit

---
+++ prac_03/randoms.py
@@ -0,0 +1,26 @@
+"""
+Randoms
+CP1404/CP5632 - Practical Extension
+"""
+
+import random
+
+# What did you see on line 1?
+# 11, 7, 9, 8
+# What was the smallest number you could have seen, what was the largest?
+# 5
+
+# What did you see on line 2?
+# 5, 7, 3
+# What was the smallest number you could have seen, what was the largest?
+# 3
+# Could line 2 have produced a 4?
+# No
+
+# What did you see on line 3?
+# 3.4288733475938917, 4.074395754713511
+# What was the smallest number you could have seen, what was the largest?
+# 2.5
+
+# Produce a random number between 1 and 100 inclusive.
+print(random.randint(1, 101))
