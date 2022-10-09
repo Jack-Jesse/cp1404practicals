6968497d154b27887fae67ff6c9b01701b80371b
Jack <jack.jesse@my.jcu.edu.au>
2022-10-09 22:19:17 +1000

    Delete old commented code.

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -7,10 +7,14 @@ A program that prints random numbers in multiple rows decided by the user.
 import random
 MINIMUM = 1
 MAXIMUM = 45
-NUMBER_OF_RANDOM_NUMBERS = 6
+NUMBERS_PER_LINE = 6
 
 number_of_rows = int(input("How many quick picks: "))
 for row in range(number_of_rows):
-    for i in range(NUMBER_OF_RANDOM_NUMBERS):
-        print(f"{random.randint(MINIMUM, MAXIMUM):> 3}", end="")
-    print("")
+    random_numbers = []
+    for i in range(NUMBERS_PER_LINE):
+        random_numbers.append(random.randint(MINIMUM, MAXIMUM))
+    quick_picks = sorted(random_numbers)
+    for pick in quick_picks:
+        print(f"{pick:>2}", end=" ")
+    print()
