55fee2a5227a5eb039d22aa41d3a2aeaaa349a6d
Jack <jack.jesse@my.jcu.edu.au>
2022-11-06 19:25:13 +1000

    Complete more guitars exercise prac_07

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -1,10 +1,9 @@
 """
 My guitars
 CP1404/CP5632 Practical
-Estimate: 30 minutes 6:00
-Actual: 44 minutes
+Estimate: 30 minutes
+Actual: 60 minutes
 """
-
 from guitar import Guitar
 
 FILENAME = "guitars.csv"
@@ -14,9 +13,14 @@ def main():
     """Sort the guitars into a sorted list."""
     guitars = []
     load_file(FILENAME, guitars)
+    name = input("Name: ")
+    year = int(input("Year: "))
+    cost = float(input("Cost: "))
+    guitars.append(Guitar(name, year, cost))
     guitars.sort()
     for guitar in guitars:
         print(guitar)
+    save_file(FILENAME, guitars)
 
 
 def load_file(filename, guitars):
@@ -26,6 +30,12 @@ def load_file(filename, guitars):
             parts = line.strip().split(",")
             guitar = Guitar(parts[0], int(parts[1]), float(parts[2]))
             guitars.append(guitar)
+
+
+def save_file(filename, guitars):
+    with open(filename, "w") as out_file:
+        for guitar in guitars:
+            print(f"{guitar.name},{guitar.year},{guitar.cost}", file=out_file)
 
 
 main()
