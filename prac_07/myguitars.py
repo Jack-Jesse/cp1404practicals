3b2d0ce3bc9cc1bbdeef23e632b3bd68f6abfeb9
Jack <jack.jesse@my.jcu.edu.au>
2022-11-06 18:14:35 +1000

    Complete load file into list

---
+++ prac_07/myguitars.py
@@ -0,0 +1,23 @@
+"""
+My guitars
+CP1404/CP5632 Practical
+Estimate: 30 minutes 6:00
+Actual:
+"""
+FILENAME = "guitars.csv"
+
+
+def main():
+    """Sort the guitars into a sorted list."""
+    guitars = []
+    load_file(FILENAME, guitars)
+
+
+def load_file(filename, guitars):
+    """Load file into a list."""
+    with open(filename, "r") as in_file:
+        for line in in_file:
+            guitars.append(line.strip().split(","))
+
+
+main()
