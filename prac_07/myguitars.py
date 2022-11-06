255f19255979bd0747bfc3f16c9265cd36e06c39
Jack <jack.jesse@my.jcu.edu.au>
2022-11-06 18:52:59 +1000

    Complete first part of more guitars prac_07

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -2,8 +2,11 @@
 My guitars
 CP1404/CP5632 Practical
 Estimate: 30 minutes 6:00
-Actual:
+Actual: 44 minutes
 """
+
+from guitar import Guitar
+
 FILENAME = "guitars.csv"
 
 
@@ -12,13 +15,17 @@ def main():
     guitars = []
     load_file(FILENAME, guitars)
     guitars.sort()
+    for guitar in guitars:
+        print(guitar)
 
 
 def load_file(filename, guitars):
     """Load file into a list."""
     with open(filename, "r") as in_file:
         for line in in_file:
-            guitars.append(line.strip().split(","))
+            parts = line.strip().split(",")
+            guitar = Guitar(parts[0], int(parts[1]), float(parts[2]))
+            guitars.append(guitar)
 
 
 main()
